bc92180a48a8c1770755c2497c92a87678a02821
AvetisM <avetis.mkhitaryants@gmail.com>
2021-04-18 19:44:27 +0300

    4. Переопределение [#503003]

--- src/main/java/ru/job4j/inheritance/JsonReport.java
+++ src/main/java/ru/job4j/inheritance/JsonReport.java
@@ -3,7 +3,7 @@ package ru.job4j.inheritance;
 public class JsonReport extends TextReport {
     @Override
     public String generate(String name, String body) {
-        return "{" + System.lineSeparator() + " " + name + ","
-                + System.lineSeparator() + " " + body + System.lineSeparator() + "}";
+        return "{" + System.lineSeparator() + " \"name\" : " + name + ","
+                + System.lineSeparator() + " \"body\" : " + body + System.lineSeparator() + "}";
     }
 }
--- src/main/java/ru/job4j/inheritance/ReportUsage.java
+++ src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -3,7 +3,7 @@ package ru.job4j.inheritance;
 public class ReportUsage {
     public static void main(String[] args) {
         JsonReport jsonReport = new JsonReport();
-        String text = jsonReport.generate("name : name", "body : body");
+        String text = jsonReport.generate("name", "body");
         System.out.println(text);
     }
 }
