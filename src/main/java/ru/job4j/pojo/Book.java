b8a45f58dc3bab1f97d5a66b540cbf541b5ed3b6
AvetisM <avetis.mkhitaryants@gmail.com>
2021-04-20 12:48:31 +0300

    3. Массивы и модели. [#503010]

---
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,27 @@
+package ru.job4j.pojo;
+
+public class Book {
+    private String title;
+    private int count;
+
+    public Book(String title, int count) {
+        this.title = title;
+        this.count = count;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,35 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book[] books = new Book[4];
+        books[0] = new Book("Head First", 300);
+        books[1] = new Book("Spring in action", 700);
+        books[2] = new Book("Clean Code", 257);
+        books[3] = new Book("Thinking in Java", 900);
+
+        for (int i = 0; i < books.length; i++) {
+            System.out.println("Title " + books[i].getTitle() + " pages " + books[i].getCount());
+        }
+
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+
+        System.out.println("Replace books");
+
+        for (int i = 0; i < books.length; i++) {
+            System.out.println("Title " + books[i].getTitle() + " pages " + books[i].getCount());
+        }
+
+        System.out.println("Show only book with title Clean Code");
+
+        for (int i = 0; i < books.length; i++) {
+            if (books[i].getTitle().equals("Clean Code")) {
+                System.out.println("Title " + books[i].getTitle()
+                        + " pages " + books[i].getCount());
+            }
+        }
+
+    }
+}
