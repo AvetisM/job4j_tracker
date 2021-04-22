546eb587f4bc1acc583e90eabc6f90b11d87c6da
AvetisM <avetis.mkhitaryants@gmail.com>
2021-04-22 18:19:17 +0300

    7. Метод удаления заявки Tracker.delete [#503014]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -29,7 +29,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (rsl) {
-            arraycopy(items, index + 1, items, index, size - index);
+            arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
