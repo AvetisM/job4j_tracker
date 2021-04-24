86ed35d385ea356acbbe382a92c32ecbd7cab1d8
AvetisM <avetis.mkhitaryants@gmail.com>
2021-04-24 20:56:02 +0300

    2.5. Реализация класса StartUI. Изменение заявки [#502946]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,5 +1,6 @@
 package ru.job4j.tracker;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class StartUI {
@@ -34,8 +35,11 @@ public class StartUI {
                 System.out.print("Enter name: ");
                 String name = scanner.nextLine();
                 Item item = new Item(name);
-                tracker.replace(id, item);
-                System.out.println("Заявка изменена успешно.");
+                if (tracker.replace(id, item)) {
+                    System.out.println("Заявка изменена успешно.");
+                } else {
+                    System.out.println("Ошибка замены заявки.");
+                }
             } else if (select == 3) {
                 System.out.println("=== Delete item ====");
                 System.out.print("Enter id: ");
