2043c7016c3c2da67123ca9ecbd9a6034c73790d
AvetisM <avetis.mkhitaryants@gmail.com>
2021-06-26 14:16:31 +0300

    2.2. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/function/LambdaUsage.java
@@ -0,0 +1,28 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaUsage {
+
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("Абрикос", "Арбуз", "Аккордеон");
+
+        Comparator<String> cmpText = (left, right) -> {
+            System.out.println("compare - " + left + " : " + right);
+           return left.compareTo(right);
+        };
+
+        list.sort(cmpText);
+
+        Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + right.length() + " : " + left.length());
+            return Integer.compare(right.length(), left.length());
+        };
+
+        list.sort(cmpDescSize);
+    }
+}
