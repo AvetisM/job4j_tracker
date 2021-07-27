707a50e2c0c49d486f0dab0422d80f002b26b6e8
AvetisM <avetis.mkhitaryants@gmail.com>
2021-07-27 07:57:30 +0300

    2. Аттестация [#49777]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -50,7 +50,6 @@ public class Analyze {
                 .flatMap(pupil -> pupil.getSubjects()
                         .stream())
                 .collect(Collectors.groupingBy(Subject::getName,
-                        LinkedHashMap::new,
                         Collectors.summingDouble(Subject::getScore)))
                 .entrySet()
                 .stream()
