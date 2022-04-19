d4da137ccaa464540ac331762f62112beec80b49
AvetisM <avetis.mkhitaryants@gmail.com>
2022-04-19 18:46:13 +0300

    1. Профилирование. [#1591 #289771]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Properties;
 
 public class StartUI {
@@ -39,7 +40,7 @@ public class StartUI {
         Output output = new ConsoleOutput();
         Input input = new ValidateInput(output, new ConsoleInput());
 
-        try (SqlTracker tracker = new SqlTracker()) {
+        /*try (SqlTracker tracker = new SqlTracker()) {
             tracker.init();
             UserAction[] actions = {
                     new CreateAction(output),
@@ -53,6 +54,32 @@ public class StartUI {
             new StartUI(output).init(input, tracker, actions);
         } catch (Exception e) {
             e.printStackTrace();
+        }*/
+
+        MemTracker memTracker = new MemTracker();
+        for (int i = 0; i < 170; i++) {
+            memTracker.add(new Item("Name" + i));
         }
+        List<Item> items = memTracker.findAll();
+        if (items.size() > 0) {
+            for (Item item : items) {
+                System.out.println(item);
+            }
+        } else {
+            System.out.println("Хранилище еще не содержит заявок");
+        }
+        for (int i = 0; i < 50; i++) {
+            memTracker.delete(i);
+        }
+        UserAction[] actions = {
+                new CreateAction(output),
+                new ShowAllItems(output),
+                new ReplaceItem(output),
+                new DeleteItem(output),
+                new FindItemById(output),
+                new FindItemByName(output),
+                new ExitProgram()
+        };
+        new StartUI(output).init(input, memTracker, actions);
     }
 }
